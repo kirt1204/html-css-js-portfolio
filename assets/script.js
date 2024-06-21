56388896f32b0540f0ab2965188105bbe107d443
kirt1204 <kirtikamra12@gmail.com>
2024-06-21 12:41:45 +0530

    Finished website

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    const menu = querySelector(".menu-links")
+    const icon = querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
